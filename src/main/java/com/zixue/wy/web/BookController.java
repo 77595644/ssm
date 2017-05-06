170f7558add6b3f1a40605d85de45f01211a75ea
wangyue <wangyue1@gomeholdings.com>
2017-05-06 12:17:59 +0800

    666

--- src/main/java/com/zixue/wy/web/BookController.java
+++ src/main/java/com/zixue/wy/web/BookController.java
@@ -8,10 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.zixue.wy.dto.AppointExecution;
 import com.zixue.wy.dto.Result;
@@ -30,11 +27,12 @@ public class BookController {
 	private BookService bookService;
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	private String list(Model model) {
+	@ResponseBody
+	private List<Book> list(Model model) {
 		List<Book> list = bookService.getList();
-		model.addAttribute("list", list);
+		// model.addAttribute("list", list);
 		// list.jsp + model = ModelAndView
-		return "list";// WEB-INF/jsp/"list".jsp
+		return list;// WEB-INF/jsp/"list".jsp
 	}
 
 	@RequestMapping(value = "/{bookId}/detail", method = RequestMethod.GET)
@@ -56,7 +54,7 @@ public class BookController {
 	@ResponseBody
 	private Result<AppointExecution> appoint(@PathVariable("bookId") Long bookId, @RequestParam("studentId") Long studentId) {
 		if (studentId == null || studentId.equals("")) {
-			return new Result<>(false, "学号不能为空");
+			return new Result<AppointExecution>(false, "学号不能为空");
 		}
 		AppointExecution execution = null;
 		try {
